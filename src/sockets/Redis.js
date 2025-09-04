63aa0d7ed60ef7a213f71deba31fc62f26468487
Adnan <111533784+adnan9760@users.noreply.github.com>
2025-09-05 00:35:24 +0530

    Update Redis.js

--- src/sockets/Redis.js
+++ src/sockets/Redis.js
@@ -1,9 +1,16 @@
 import { createClient } from "redis";
 
-const redis = createClient();
+// Remote Redis URL
+const REDIS_URL = "redis://default:Eq8eXdUZ6PXsqi4o76aLu8HuHAUuOmOL@redis-17683.c14.us-east-1-2.ec2.redns.redis-cloud.com:17683";
+
+const redis = createClient({
+  url: REDIS_URL
+});
 
 redis.on("error", (err) => console.error("Redis Client Error", err));
 
-await redis.connect();   
+await redis.connect();
+
+console.log("Connected to remote Redis!");
 
 export default redis;
